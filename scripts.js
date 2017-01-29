39b189adc3a97e4382f9e51ca8d87d18785ade5a
Nitin Koli <nitin@D110008.local>
2017-01-29 16:48:13 +0530

    updates

--- scripts.js
+++ scripts.js
@@ -22,7 +22,7 @@ function init() {
     var listHTML = document.getElementById('list_container');
     for(i=0;i<workList.length;i++) {
       // console.log(workList.length);
-      listHTML.innerHTML += '<a href="' + workList[i].link + '" target="_blank" class="row"><div class="row__details"><h2 class="title">' + workList[i].title + '</h2>  <p class="row__details_brief">' + workList[i].brief + '</p>  <div class="row__details_other">    <div class="row__details_other__text">Type: <p>' + workList[i].type + '</p></div>    <div class="row__details_other__text">Preview: <p>' + workList[i].preview + '</p></div>    <div class="row__details_other__text">Platform: <p>' + workList[i].platform + '</p></div>  </div></div><div class="row__arrow">&#x2197;</div></a>';
+      listHTML.innerHTML += '<a href="' + workList[i].link + '" target="_blank" class="row"><div class="row__details"><h2 class="title">' + workList[i].title + '</h2>  <p class="row__details_brief">' + workList[i].brief + '</p>  <div class="row__details_other">    <div class="row__details_other__text">Type: <p>' + workList[i].type + '</p></div>    <div class="row__details_other__text">Preview: <p>' + workList[i].preview + '</p></div>    <div class="row__details_other__text">Platform: <p>' + workList[i].platform + '</p></div>  </div></div><div class="row__arrow"><svg height="48" viewBox="0 0 48 48" width="32"><path d="M0 0h48v48h-48z" fill="none"/><path fill="#0072c2" d="M38 38h-28v-28h14v-4h-14c-2.21 0-4 1.79-4 4v28c0 2.21 1.79 4 4 4h28c2.21 0 4-1.79 4-4v-14h-4v14zm-10-32v4h7.17l-19.66 19.66 2.83 2.83 19.66-19.66v7.17h4v-14h-14z"/></svg></div></a>';
 
       // FOR VISITED STATE
       var rows = document.querySelectorAll('a.row');
